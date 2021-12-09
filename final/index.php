135980b3b880d25d854b641a3fa90a8ae675c294
TusharRai99 <72166628+TusharRai99@users.noreply.github.com>
2021-12-08 21:23:41 -0500

    Final V1

---
+++ final/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TR Recipes</title>
+</head>
+
+<body>
+    <h1>Work</h1>
+    <h2>sdfs</h2>
+</body>
+
+</html>
